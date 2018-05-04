2bfb4f622fd3548049d1f8393ba29bd4e6df9d85
James Kaviyil Jose <jamesjose03@gmail.com>
2018-05-04 23:39:17 +0530

    Setting up the Registration Page

---
+++ tab-tracker/app/src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+<h1>Register</h1>
+<input type="email" name="email" placeholder="Email" v-model="email"/>
+
+<br>
+<input type="password" name="password" placeholder="Password" v-model="password"/>
+<br>
+<button>Register</button>
+</div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
